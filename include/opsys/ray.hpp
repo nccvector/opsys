ed3b8155a8b701ba394bd2af7242cd18fa1a65a8
nccvector <fzn1020@gmail.com>
2026-06-28 23:26:11 +0900

    Require floating ray fields

--- include/opsys/ray.hpp
+++ include/opsys/ray.hpp
@@ -8,6 +8,9 @@ namespace opsys {
 
 namespace detail {
 
+template <typename T>
+concept floating_field = std::floating_point<std::remove_cvref_t<T>>;
+
 template <typename Ray>
 using ray_scalar_t = std::common_type_t<
     std::remove_cvref_t<decltype(std::declval<const Ray&>().ox)>,
@@ -26,13 +29,13 @@ using ray_scalar_t = detail::ray_scalar_t<Ray>;
 template <typename Ray>
 concept SpectralRay = requires(Ray ray, const Ray const_ray, ray_scalar_t<Ray> value) {
     requires std::floating_point<ray_scalar_t<Ray>>;
-    { const_ray.ox } -> std::convertible_to<ray_scalar_t<Ray>>;
-    { const_ray.oy } -> std::convertible_to<ray_scalar_t<Ray>>;
-    { const_ray.oz } -> std::convertible_to<ray_scalar_t<Ray>>;
-    { const_ray.dx } -> std::convertible_to<ray_scalar_t<Ray>>;
-    { const_ray.dy } -> std::convertible_to<ray_scalar_t<Ray>>;
-    { const_ray.dz } -> std::convertible_to<ray_scalar_t<Ray>>;
-    { const_ray.wavelength } -> std::convertible_to<ray_scalar_t<Ray>>;
+    requires detail::floating_field<decltype(const_ray.ox)>;
+    requires detail::floating_field<decltype(const_ray.oy)>;
+    requires detail::floating_field<decltype(const_ray.oz)>;
+    requires detail::floating_field<decltype(const_ray.dx)>;
+    requires detail::floating_field<decltype(const_ray.dy)>;
+    requires detail::floating_field<decltype(const_ray.dz)>;
+    requires detail::floating_field<decltype(const_ray.wavelength)>;
 
     ray.ox = value;
     ray.oy = value;
